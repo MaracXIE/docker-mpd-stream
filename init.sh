8a2383c6cf29f1bc5bc621c1358a37411cd39534
Neil Castelino <neil.fl93@gmail.com>
2016-06-29 16:20:27 +0000

    use vars

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Modify these accordingly
+music_dir="/home/neil/Music"
+playlist_dir="/dev/shm/"
+mpd_dir="/dev/shm/"
+
 docker build -t mpdocker .
-container=$(docker run -dit -p 6600:6600 -p 8000:8000 -v /home/neil/Music:/music -v /dev/shm:/playlists -v /dev/shm:/mpd mpdocker)
+container=$(docker run -dit -p 6600:6600 -p 8000:8000 -v "$music_dir":/music -v "$playlist_dir":/playlists -v "$mpd_dir":/mpd mpdocker)
 echo "$container"
 docker exec -it "$container" bash
